f2544e29e73d826b987e78da48ae77aac4107129
Nick Müller <nick.mueller@uni-greifswald.de>
2022-12-25 21:36:51 +0100

    Deps updated

--- src/main/java/de/milchreisjunkie/netpetcam/NetPetcam.java
+++ src/main/java/de/milchreisjunkie/netpetcam/NetPetcam.java
@@ -1,11 +1,13 @@
 package de.milchreisjunkie.netpetcam;
 
+import java.awt.Dimension;
 import java.util.concurrent.Executors;
 
+import com.github.sarxos.webcam.WebcamStreamer;
+import com.github.sarxos.webcam.ds.v4l4j.V4l4jDriver;
 import org.apache.log4j.Logger;
 
 import com.github.sarxos.webcam.Webcam;
-import com.github.sarxos.webcam.WebcamStreamer;
 import com.hopding.jrpicam.exceptions.FailedToRunRaspistillException;
 
 import de.milchreisjunkie.netpetcam.dropbox.DropboxManager;
@@ -17,7 +19,7 @@ public class NetPetcam {
 
 	private Config config;
 	private DropboxManager dbxMan;
-
+	
 	public NetPetcam(Config config) {
 		this.config = config;
 	}
@@ -46,6 +48,10 @@ public class NetPetcam {
 	}
 
 	private void startStream() {
+		Webcam.setDriver(V4l4jDriver.class);
+		Webcam cam = Webcam.getDefault();
+		cam.setViewSize(new Dimension(640, 480));
+
 		new WebcamStreamer(8080, Webcam.getDefault(), 20, true);
 		do {
 			try {
--- src/main/java/de/milchreisjunkie/netpetcam/interfaces/ICaptureDevice.java
+++ src/main/java/de/milchreisjunkie/netpetcam/interfaces/ICaptureDevice.java
@@ -2,6 +2,7 @@ package de.milchreisjunkie.netpetcam.interfaces;
 
 public interface ICaptureDevice {
 
-	public void setup(Object options) throws Exception;
-	public void capture() throws Exception;
+	void setup(Object options) throws Exception;
+	void capture() throws Exception;
+	
 }
